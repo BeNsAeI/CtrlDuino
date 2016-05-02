4b6432ae503dbf0b0e0d33f78bf5b9daabcf0e65
bensaei <bensaei@github.com>
2016-05-02 14:08:39 -0700

    Fixing bugs

--- GPad-no-joystick/GPad-no-joystick.ino
+++ GPad-no-joystick/GPad-no-joystick.ino
@@ -1,44 +1,160 @@
-/* Read Jostick
-  * ------------
-  *
-  * Reads two analog pins that are supposed to be
-  * connected to a jostick made of two potentiometers
-  *
-  * We send three bytes back to the comp: one header and two
-  * with data as signed bytes, this will take the form:
-  *     Jxy\r\n
-  *
-  * x and y are integers and sent in ASCII 
-  * 
-  * http://www.0j0.org | http://arduino.berlios.de
-  * copyleft 2005 DojoDave for DojoCorp
-  */
+ void setup()   {                
 
- int ledPin = 13;
- int joyPin1 = PIN_F7;                 // slider variable connecetd to analog pin 0
- int joyPin2 = PIN_B6;                 // slider variable connecetd to analog pin 1
- int value1 = 0;                  // variable to read the value from the analog pin 0
- int value2 = 0;                  // variable to read the value from the analog pin 1
-
- void setup() {
-  pinMode(ledPin, OUTPUT);              // initializes digital pins 0 to 7 as outputs
-  Serial.begin(9600);
+    Serial.begin(9600);
+   pinMode(PIN_F0, INPUT_PULLUP);
+   pinMode(PIN_F1, INPUT_PULLUP);
+   pinMode(PIN_F4, INPUT_PULLUP);
+   pinMode(PIN_F5, INPUT_PULLUP);
+   pinMode(PIN_F6, INPUT_PULLUP);
+   pinMode(PIN_F7, INPUT_PULLUP);
+   pinMode(PIN_B6, INPUT_PULLUP);
+   pinMode(PIN_B5, INPUT_PULLUP);
+   pinMode(PIN_B4, INPUT_PULLUP);
+   pinMode(PIN_D7, INPUT_PULLUP);
+   pinMode(PIN_B0, INPUT_PULLUP);
+   pinMode(PIN_B1, INPUT_PULLUP);
+   pinMode(PIN_B2, INPUT_PULLUP);
+   pinMode(PIN_B3, INPUT_PULLUP);
+   pinMode(PIN_D6, OUTPUT);
+ }
+ void loop()                     
+ {
+   int DL = 50;
+   digitalWrite(PIN_D6,HIGH);
+   while(true)
+   {
+     delay(DL);
+     if (!digitalRead(PIN_F0))
+    {
+      Keyboard.press(KEY_I);
+    }   else
+    {
+     Keyboard.release(KEY_I);
+    }
+    if (!digitalRead(PIN_F1))
+    {
+      Keyboard.press(KEY_J);
+    }   else
+    {
+     Keyboard.release(KEY_J);
+    }
+    if (!digitalRead(PIN_F4))
+    {
+      Keyboard.press(KEY_L);
+    }   else
+    {
+     Keyboard.release(KEY_L);
+    }
+    if (!digitalRead(PIN_F5))
+    {
+      Keyboard.press(KEY_K);
+    }   else
+    {
+     Keyboard.release(KEY_K);
+    }
+    if (!digitalRead(PIN_F6))
+    {
+      Keyboard.press(KEY_S);
+    }   else
+    {
+     Keyboard.release(KEY_S);
+    }
+    if (!digitalRead(PIN_F7))
+    {
+      Keyboard.press(KEY_D);
+    }   else
+    {
+     Keyboard.release(KEY_D);
+    }
+    if (!digitalRead(PIN_B6))
+    {
+      Keyboard.press(KEY_A);
+    }   else
+    {
+     Keyboard.release(KEY_A);
+    }
+    if (!digitalRead(PIN_B5))
+    {
+      Keyboard.press(KEY_W);
+    }   else
+    {
+     Keyboard.release(KEY_W);
+    }
+    if (!digitalRead(PIN_B4))
+    {
+      Keyboard.press(KEY_F);
+    }   else
+    {
+     Keyboard.release(KEY_F);
+    }
+    if (!digitalRead(PIN_D7))
+    {
+      Keyboard.press(KEY_H);
+    }   else
+    {
+     Keyboard.release(KEY_H);
+    }
+    if (!digitalRead(PIN_B0))
+    {
+      Keyboard.press(KEY_Q);
+    }   else
+    {
+     Keyboard.release(KEY_Q);
+    }
+    if (!digitalRead(PIN_B1))
+    {
+      Keyboard.press(KEY_E);
+    }   else
+    {
+     Keyboard.release(KEY_E);
+    }
+    if (!digitalRead(PIN_B2))
+    {
+      Keyboard.press(KEY_U);
+    }   else
+    {
+     Keyboard.release(KEY_U);
+    }
+    if (!digitalRead(PIN_B3))
+    {
+      Keyboard.press(KEY_O);
+    }   else
+    {
+     Keyboard.release(KEY_O);
+    }
+    if( 
+    digitalRead(PIN_F0) &&
+    digitalRead(PIN_F1) &&
+    digitalRead(PIN_F4) &&
+    digitalRead(PIN_F5) &&
+    digitalRead(PIN_F6) &&
+    digitalRead(PIN_F7) &&
+    digitalRead(PIN_B6) &&
+    digitalRead(PIN_B5) &&
+    digitalRead(PIN_B4) &&
+    digitalRead(PIN_D7) &&
+    digitalRead(PIN_B0) &&
+    digitalRead(PIN_B1) &&
+    digitalRead(PIN_B2) &&
+    digitalRead(PIN_B3)
+      )
+    {
+       break;
+    }
+   }
+   Keyboard.release(KEY_I);
+   Keyboard.release(KEY_J);
+   Keyboard.release(KEY_L);
+   Keyboard.release(KEY_K);
+   Keyboard.release(KEY_S);
+   Keyboard.release(KEY_D);
+   Keyboard.release(KEY_A);
+   Keyboard.release(KEY_W);
+   Keyboard.release(KEY_F);
+   Keyboard.release(KEY_H);
+   Keyboard.release(KEY_Q);
+   Keyboard.release(KEY_E);
+   Keyboard.release(KEY_U);
+   Keyboard.release(KEY_O);
  }
 
- int treatValue(int data) {
-  return data;
- }
-
- void loop() {
-  // reads the value of the variable resistor 
-  value1 = analogRead(joyPin1);   
-  // this small pause is needed between reading
-  // analog pins, otherwise we get the same value twice
-  delay(100);             
-  // reads the value of the variable resistor 
-  value2 = analogRead(joyPin2);   
-  
-  Serial.print(treatValue(value1));
-  Serial.print(", ");
-  Serial.println(treatValue(value2));
- }
